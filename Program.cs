d4adff765d81e6f760848a041946254e661f5008
Jason Ballejo <ballejo19@gmail.com>
2023-09-05 15:39:06 -0400

    setup

--- Program.cs
+++ Program.cs
@@ -1,2 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, Worlds!");
+Console.WriteLine("Hello, C# Programmers!");
+Console.WriteLine("Hello, C# Programmers!");
+Console.Write("First");
+Console.Write("Second");
+Console.WriteLine("Hello, C# Programmers!");
+Console.Write("Third");
+
+Console.WriteLine("Hello, C# Programmers!");
