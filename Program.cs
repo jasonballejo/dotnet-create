82ab18d21f95d9c3c168028723e70481a410af59
Jason Ballejo <ballejo19@gmail.com>
2023-09-06 15:23:09 -0400

    Arrays - Dimensional

--- Program.cs
+++ Program.cs
@@ -20,39 +20,39 @@ namespace HelloJason
             // //// 00001000 = 8
  
             // 1 byte (8 bit) unsigned, where signed means it can be negative
-            byte myByte = 255;
-            byte mySecondByte = 0;
+            // byte myByte = 255;
+            // byte mySecondByte = 0;
  
-            // 1 byte (8 bit) signed, where signed means it can be negative
-            sbyte mySbyte = 127;
-            sbyte mySecondSbyte = -128;
- 
- 
-            // 2 byte (16 bit) unsigned, where signed means it can be negative
-            ushort myUshort = 65535;
- 
-            // 2 byte (16 bit) signed, where signed means it can be negative
-            short myShort = -32768;
- 
-            // 4 byte (32 bit) signed, where signed means it can be negative
-            int myInt = 2147483647;
-            int mySecondInt = -2147483648;
- 
-            // 8 byte (64 bit) signed, where signed means it can be negative
-            long myLong = -9223372036854775808;
+            // // 1 byte (8 bit) signed, where signed means it can be negative
+            // sbyte mySbyte = 127;
+            // sbyte mySecondSbyte = -128;
  
  
-            // 4 byte (32 bit) floating point number
-            float myFloat = 0.751f;
-            float mySecondFloat = 0.75f;
+            // // 2 byte (16 bit) unsigned, where signed means it can be negative
+            // ushort myUshort = 65535;
  
-            // 8 byte (64 bit) floating point number
-            double myDouble = 0.751;
-            double mySecondDouble = 0.75d;
+            // // 2 byte (16 bit) signed, where signed means it can be negative
+            // short myShort = -32768;
  
-            // 16 byte (128 bit) floating point number
-            decimal myDecimal = 0.751m;
-            decimal mySecondDecimal = 0.75m;
+            // // 4 byte (32 bit) signed, where signed means it can be negative
+            // int myInt = 2147483647;
+            // int mySecondInt = -2147483648;
+ 
+            // // 8 byte (64 bit) signed, where signed means it can be negative
+            // long myLong = -9223372036854775808;
+ 
+ 
+            // // 4 byte (32 bit) floating point number
+            // float myFloat = 0.751f;
+            // float mySecondFloat = 0.75f;
+ 
+            // // 8 byte (64 bit) floating point number
+            // double myDouble = 0.751;
+            // double mySecondDouble = 0.75d;
+ 
+            // // 16 byte (128 bit) floating point number
+            // decimal myDecimal = 0.751m;
+            // decimal mySecondDecimal = 0.75m;
  
             // Console.WriteLine(myFloat - mySecondFloat);
             // Console.WriteLine(myDouble - mySecondDouble);
@@ -60,12 +60,50 @@ namespace HelloJason
  
  
  
-            string myString = "Hello World";
-            // Console.WriteLine(myString);
-            string myStringWithSymbols = "!@#$@^$%%^&(&%^*__)+%^@##$!@%123589071340698ughedfaoig137";
+            // string myString = "Hello World";
+            // // Console.WriteLine(myString);
+            // string myStringWithSymbols = "!@#$@^$%%^&(&%^*__)+%^@##$!@%123589071340698ughedfaoig137";
             // Console.WriteLine(myStringWithSymbols);
  
-            bool myBool = true;
+            // bool myBool = true;
+
+            string[] myGroceryArray = new string[2]; 
+
+            myGroceryArray[0] = "Tomato";
+            myGroceryArray[1] = "Rice";
+
+            // Console.WriteLine(myGroceryArray[0]);
+            // Console.WriteLine(myGroceryArray[1]);
+            // Console.WriteLine(myGroceryArray[2]);
+
+            string[] mySecondGroceryArray = {"Apples", "Eggs"};
+
+            // mySecondGroceryArray[2] = "";
+
+            // Console.WriteLine(mySecondGroceryArray[0]);
+            // Console.WriteLine(mySecondGroceryArray[1]);
+
+            List<string> myGroceryList = new List<string>() { "Milk", "Cheese"};
+
+            // Console.WriteLine(myGroceryList[0]);
+            // Console.WriteLine(myGroceryList[1]);
+
+            // Console.WriteLine(myGroceryList[2]);
+
+            myGroceryList.Add("Oranges");
+
+            // Console.WriteLine(myGroceryList[2]);
+
+            IEnumerable<string> myGroceryIEnumberable = myGroceryList;
+
+            Console.WriteLine(myGroceryIEnumberable.First());
+
+            string[,] myTwoDimensionalArray = new string[,] {
+                {"Apples", "Eggs"},
+                {"Milk", "Cheese"},
+            };
+
+            Console.WriteLine(myTwoDimensionalArray[1,1]);
         }
     }
 }
