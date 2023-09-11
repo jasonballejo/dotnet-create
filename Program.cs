e9d58b75a91838cb82e1eae9d5b911ef6a79c729
Jason Ballejo <ballejo19@gmail.com>
2023-09-11 16:05:57 -0400

    Data structures

--- Program.cs
+++ Program.cs
@@ -6,110 +6,64 @@ namespace HelloJason
     {
         static void Main(string[] args)
         {
-            //// 1 byte is made up of 8 bits 00000000 - these bits can be used to store a number as follows
-            // //// Each bit can be worth 0 or 1 of the value it is placed in
-            // ////// From the right we start with a value of 1 and double for each digit to the left
-            // //// 00000000 = 0
-            // //// 00000001 = 1
-            // //// 00000010 = 2
-            // //// 00000011 = 3
-            // //// 00000100 = 4
-            // //// 00000101 = 5
-            // //// 00000110 = 6
-            // //// 00000111 = 7
-            // //// 00001000 = 8
- 
-            // 1 byte (8 bit) unsigned, where signed means it can be negative
-            // byte myByte = 255;
-            // byte mySecondByte = 0;
- 
-            // // 1 byte (8 bit) signed, where signed means it can be negative
-            // sbyte mySbyte = 127;
-            // sbyte mySecondSbyte = -128;
- 
- 
-            // // 2 byte (16 bit) unsigned, where signed means it can be negative
-            // ushort myUshort = 65535;
- 
-            // // 2 byte (16 bit) signed, where signed means it can be negative
-            // short myShort = -32768;
- 
-            // // 4 byte (32 bit) signed, where signed means it can be negative
-            // int myInt = 2147483647;
-            // int mySecondInt = -2147483648;
- 
-            // // 8 byte (64 bit) signed, where signed means it can be negative
-            // long myLong = -9223372036854775808;
- 
- 
-            // // 4 byte (32 bit) floating point number
-            // float myFloat = 0.751f;
-            // float mySecondFloat = 0.75f;
- 
-            // // 8 byte (64 bit) floating point number
-            // double myDouble = 0.751;
-            // double mySecondDouble = 0.75d;
- 
-            // // 16 byte (128 bit) floating point number
-            // decimal myDecimal = 0.751m;
-            // decimal mySecondDecimal = 0.75m;
- 
-            // Console.WriteLine(myFloat - mySecondFloat);
-            // Console.WriteLine(myDouble - mySecondDouble);
-            // Console.WriteLine(myDecimal - mySecondDecimal);
- 
- 
- 
-            // string myString = "Hello World";
-            // // Console.WriteLine(myString);
-            // string myStringWithSymbols = "!@#$@^$%%^&(&%^*__)+%^@##$!@%123589071340698ughedfaoig137";
-            // Console.WriteLine(myStringWithSymbols);
- 
-            // bool myBool = true;
+            // int myInt = 5;
+            // int mySecondInt = 10;
 
-            string[] myGroceryArray = new string[2]; 
+            // Console.WriteLine(myInt.Equals(mySecondInt));
+            // Console.WriteLine(myInt.Equals(mySecondInt / 2));
 
-            myGroceryArray[0] = "Tomato";
-            myGroceryArray[1] = "Rice";
+            // Console.WriteLine(myInt != mySecondInt);
+            // Console.WriteLine(myInt != mySecondInt / 2);
+            // Console.WriteLine(myInt != mySecondInt / 3);
 
-            // Console.WriteLine(myGroceryArray[0]);
-            // Console.WriteLine(myGroceryArray[1]);
-            // Console.WriteLine(myGroceryArray[2]);
+            // myInt++;
 
-            string[] mySecondGroceryArray = {"Apples", "Eggs"};
+            // Console.WriteLine(myInt);
 
-            // mySecondGroceryArray[2] = "";
+            // myInt += 7;
 
-            // Console.WriteLine(mySecondGroceryArray[0]);
-            // Console.WriteLine(mySecondGroceryArray[1]);
+            // Console.WriteLine(myInt);
+            
+            // myInt -= 8;
 
-            List<string> myGroceryList = new List<string>() { "Milk", "Cheese"};
+            // Console.WriteLine(myInt);
 
-            // Console.WriteLine(myGroceryList[0]);
-            // Console.WriteLine(myGroceryList[1]);
+            // Console.WriteLine(myInt * mySecondInt);
 
-            // Console.WriteLine(myGroceryList[2]);
+            // Console.WriteLine(mySecondInt / myInt);
 
-            myGroceryList.Add("Oranges");
+            // Console.WriteLine(mySecondInt + myInt);
 
-            // Console.WriteLine(myGroceryList[2]);
+            // Console.WriteLine(myInt - mySecondInt);
 
-            IEnumerable<string> myGroceryIEnumberable = myGroceryList;
+            // Console.WriteLine(5 + 5 * 10);
 
-            // Console.WriteLine(myGroceryIEnumberable.First());
+            // Console.WriteLine((5 + 5) * 10);
 
-            string[,] myTwoDimensionalArray = new string[,] {
-                {"Apples", "Eggs"},
-                {"Milk", "Cheese"},
-            };
+            // Console.WriteLine(Math.Pow(5, 4));
 
-            // Console.WriteLine(myTwoDimensionalArray[1,1]);
+            // Console.WriteLine(Math.Sqrt(25));
 
-            Dictionary<string, string[]> myGroceryDictionary = new Dictionary<string, string[]>(){
-                {"Dairy", new string[]{"Cheese", "Milk", "Eggs"}}
-            };
+            Console.WriteLine(5 < 50 && 5 > 10);
 
-            Console.WriteLine(myGroceryDictionary["Dairy"][2]);
+            // string myString = "Test";
+
+            // Console.WriteLine(myString);
+
+            // myString += ". second part.";
+
+            // Console.WriteLine(myString);
+
+            // myString = myString + " \"third\\ part.";
+
+            // Console.WriteLine(myString);
+
+            // string[] myStringArray = myString.Split(". ");
+            // Console.WriteLine(myStringArray[0]);
+            // Console.WriteLine(myStringArray[1]);
+            // Console.WriteLine(myStringArray[2]);
+
+
         }
     }
 }
