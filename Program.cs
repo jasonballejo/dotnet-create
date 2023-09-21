27d1d2a67a276ade1223ccf08eb2c6d83c10305d
Jason Ballejo <ballejo19@gmail.com>
2023-09-21 14:21:16 -0400

    if statement

--- Program.cs
+++ Program.cs
@@ -6,64 +6,16 @@ namespace HelloJason
     {
         static void Main(string[] args)
         {
-            // int myInt = 5;
-            // int mySecondInt = 10;
+            int myInt = 5;
+            int mySecondInt = 10;
 
-            // Console.WriteLine(myInt.Equals(mySecondInt));
-            // Console.WriteLine(myInt.Equals(mySecondInt / 2));
+//If true run the block of code, if false then skip
+            if (myInt < mySecondInt)
+            {
+                myInt += 10;
+            }  
 
-            // Console.WriteLine(myInt != mySecondInt);
-            // Console.WriteLine(myInt != mySecondInt / 2);
-            // Console.WriteLine(myInt != mySecondInt / 3);
-
-            // myInt++;
-
-            // Console.WriteLine(myInt);
-
-            // myInt += 7;
-
-            // Console.WriteLine(myInt);
-            
-            // myInt -= 8;
-
-            // Console.WriteLine(myInt);
-
-            // Console.WriteLine(myInt * mySecondInt);
-
-            // Console.WriteLine(mySecondInt / myInt);
-
-            // Console.WriteLine(mySecondInt + myInt);
-
-            // Console.WriteLine(myInt - mySecondInt);
-
-            // Console.WriteLine(5 + 5 * 10);
-
-            // Console.WriteLine((5 + 5) * 10);
-
-            // Console.WriteLine(Math.Pow(5, 4));
-
-            // Console.WriteLine(Math.Sqrt(25));
-
-            Console.WriteLine(5 < 50 && 5 > 10);
-
-            // string myString = "Test";
-
-            // Console.WriteLine(myString);
-
-            // myString += ". second part.";
-
-            // Console.WriteLine(myString);
-
-            // myString = myString + " \"third\\ part.";
-
-            // Console.WriteLine(myString);
-
-            // string[] myStringArray = myString.Split(". ");
-            // Console.WriteLine(myStringArray[0]);
-            // Console.WriteLine(myStringArray[1]);
-            // Console.WriteLine(myStringArray[2]);
-
-
+            Console.WriteLine(myInt);
         }
     }
 }
